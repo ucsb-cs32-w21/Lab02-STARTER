326730589e68843c42d870a66c4115885f607152
Zoe Wood <zwood@calpoly.edu>
2021-01-12 21:17:36 -0800

    Create stateDemog.cpp

---
+++ stateDemog.cpp
@@ -0,0 +1,4 @@
+/*
+  class to represent state data - fairly redundent at this point will be improved
+  See lab descirption for more information on filling in the implementation of the state demographic data
+*/
