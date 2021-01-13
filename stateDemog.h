2f755ba13f2977a6e71997e4c8dfb6495b23f5f5
Zoe Wood <zwood@calpoly.edu>
2021-01-12 21:17:13 -0800

    Create stateDemog.h

---
+++ stateDemog.h
@@ -0,0 +1,6 @@
+/*
+  class to represent state data - fairly redundent at this point will be improved
+  See lab descirption for more information on filling in the implementation of the state demographic data
+*/
+
+  
